a67176c0869eccc3da3078e231e1d54ff3870d6a
Johan <johan@groenewald.co.za>
2018-06-20 01:56:37 +0200

    Finished initial gateway toggle script

--- toggle.py
+++ toggle.py
@@ -6,8 +6,10 @@
 from __future__ import print_function
 import sys
 import argparse
+from collections import OrderedDict
 
 from master import OUTFILE, EOL
+
 
 def main(arguments):
     parser = argparse.ArgumentParser(
@@ -15,42 +17,74 @@ def main(arguments):
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument(
-        '-c', '--client', help="Client name", type=str
+        '-c', '--computer', help="Computer name", type=str
     )
     parser.add_argument(
         '-g', '--gateway', help="Gateway", type=str
     )
 
     args = parser.parse_args(arguments)
-    show = args.client is None and args.gateway is None
-
-    host_mask = 'host '
-    gateway_mask = 'option routers '
 
     with open(OUTFILE) as f:
         buffer = f.read()
+    if len(buffer) == 0:
+        print('{} is empty'.format(OUTFILE))
+        return
     lines = buffer.split(EOL)
-    state = 0
-    m = ''
 
     def clean(line, exclude):
         return line[:-1].replace(exclude, '').replace(' ', '')
 
-    for line in lines:
-        h = line.startswith(host_mask)
-        o = line.find(gateway_mask)
-        if h and state == 0:
-            m = clean(line, host_mask)
-            state = 1
-        elif h and state == 1:
-            print('NoGateway {}'.format(m))
-            m = clean(line, host_mask)
-            state = 0
-        elif o >= 0 and state == 1:
-            print('{} {}'.format(
-                clean(line, gateway_mask), m
-            ))
-            state = 0
+    show = args.computer is None and args.gateway is None
+    name = args.computer is not None
+    gateway = args.gateway is not None
+    host_pattern = 'host '
+    gateway_pattern = '  option routers '
+    no_gateway_message = '<NoGateway>'
+
+    if show:
+        computers = []
+        for line in lines:
+            computer = line.startswith(host_pattern)
+            if computer:
+                computers.append(clean(line, host_pattern))
+        for computer in computers:
+            print(computer)
+    elif name:
+        named_computer = None
+        computer_info = []
+        for i, line in enumerate(lines):
+            if named_computer is None:
+                computer = line.startswith(
+                    '{}{}'.format(host_pattern, args.computer)
+                )
+                if computer:
+                    named_computer = line
+            else:
+                end_brace = line == '}'
+                computer_info.append(i)
+                if end_brace:
+                    break
+        if named_computer is None:
+            print('Computer Not found')
+        else:
+            print(named_computer)
+            gateway_updated = False
+            for i in computer_info:
+                print(lines[i])
+                info_gateway = lines[i].startswith(gateway_pattern)
+                if info_gateway and gateway:
+                    lines[i] = '{}{};'.format(gateway_pattern, args.gateway)
+                    gateway_updated = True
+            if gateway_updated:
+                print(named_computer)
+                for i in computer_info:
+                    print(lines[i])
+                with open(OUTFILE, 'w') as f:
+                    f.write(EOL.join(lines))
+                print('<dhcp.conf successfully updated>')
+            else:
+                print(no_gateway_message)
 
 
 if __name__ == '__main__':
